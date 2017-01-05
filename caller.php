e043a0b26575879cd1c750e07edeb9a226f89a20
n-castillo <manklar@hotmail.com>
2017-01-05 15:35:51 -0600

    First commit

---
+++ caller.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nelson_castillo
+ * Date: 8/24/15
+ * Time: 11:34 AM
+ */
+/*
+Plugin Name: Twilio Manager
+Plugin URI: http://www.paulbunyan.net
+Description:  Loader for Twilio manager app
+Author: Nelson Castillo
+Version: 0.01
+Author URI: http://www.castillonelson.com
+*/
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+global $db_functions;
+
+function gzLoaderAddCss()
+{
+    echo '<link rel="stylesheet" media="all" href="' . plugin_dir_url(__FILE__) . 'css/twl-manager.css" type="text/css" />';
+}
+
+add_action('admin_head', 'gzLoaderAddCss');
+
+function gzLoader()
+{
+    $title = __(get_plugin_data(__FILE__)['Name']);
+    $url = 'http://'.$_SERVER['HTTP_HOST']."/app/manage/";
+    echo
+        '<div class="wrap twl-wrap">
+            <h1>' . esc_html($title) . '</h1>
+            <div class="twl-iframe-wrap"> 
+                <iframe src="/app/manage/game" id="theIframe" class="twl-iframe"></iframe>
+            </div>
+        </div>
+        ';
+
+    return null;
+}
+
+function twlPluginMenu()
+{
+    $icon = '';
+    if (file_exists(__DIR__ . '/logo/twilio_logo.svg')) {
+        $icon = 'data:image/svg+xml;base64,' . base64_encode(
+                file_get_contents(
+                    __DIR__ . '/logo/twilio_logo.svg'
+                )
+            );
+
+    }
+
+    add_menu_page(
+        'Twilio Manager',
+        'Twilio Manager',
+        'manage_options',
+        'twl-loader',
+        'gzLoader',
+        $icon
+    );
+
+}
+
+add_action('admin_menu', 'twlPluginMenu');
+
+
+global $twilio_pbc_twilio_pbc_db_version;
+$twilio_pbc_twilio_pbc_db_version = '1.0';
+
+function jal_install() {
+    global $wpdb;
+    global $twilio_pbc_twilio_pbc_db_version;
+    $table_name = $wpdb->prefix . 'twilio_pbc';
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+		id mediumint(15) NOT NULL AUTO_INCREMENT,
+		time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+		name tinytext NOT NULL,
+		text text NOT NULL,
+		number varchar(55) DEFAULT '' NOT NULL,
+		PRIMARY KEY  (id)
+	) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+
+    add_option( 'twilio_pbc_db_version', $twilio_pbc_twilio_pbc_db_version );
+
+    $cmd = "composer require twilio/sdk";
+    exec($cmd . " > /dev/null &");
+
+
+}
+function jal_uninstall() {
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . 'twilio_pbc';
+
+    $sql = "DROP TABLE $table_name;";
+
+    $wpdb->query($sql);
+    delete_option("my_plugin_db_version");
+
+    $cmd = "composer remove twilio/sdk --update-with-dependencies";
+    exec($cmd . " > /dev/null &");
+
+}
+function myplugin_update_db_check() {
+    global $twilio_pbc_db_version;
+    if ( get_site_option( 'twilio_pbc_db_version' ) != $twilio_pbc_db_version ) {
+        jal_install();
+    }
+}
+add_action( 'plugins_loaded', 'myplugin_update_db_check' );
+
+register_activation_hook( __FILE__, 'jal_install' );
+register_deactivation_hook( __FILE__, 'jal_uninstall' );
---
+++ src/functions.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nelson_castillo
+ * Date: 1/3/17
+ * Time: 17:29
+ */
+
+namespace Pbc\Twilio;
+require_once dirname(dirname(dirname(dirname(dirname(__DIR__))))) .'/vendor/autoload.php';
+/**
+ * Class GetMail
+ * @package Pbc\Mail
+ */
+class PBC_Twilio
+{
+
+    /**
+     * @var string
+     */
+    private $client = '';
+    /**
+     * @return string
+     */
+    private function getClient()
+    {
+
+        return $this->client;
+    }
+    /**
+     * @param string
+     */
+    private function setClient()
+    {
+
+        $this->client = new Twilio\Rest\Client($this->getSID(), $this->getToken());
+    }
+    /**
+     * @var string
+     */
+    private $sid = '';
+    /**
+     * @return string
+     */
+    private function getSID()
+    {
+        return $this->sid;
+    }
+    /**
+     * @param string $SID
+     */
+    public function setSID($SID)
+    {
+        return $this->sid = $SID;
+    }
+    /**
+     * @var string
+     */
+    private $token = '';
+    /**
+     * @return string
+     */
+    private function getToken()
+    {
+        return $this->token;
+    }
+    /**
+     * @param string $token
+     */
+    public function setToken($token)
+    {
+        return $this->token = $token;
+    }
+    /**
+     * @var string
+     */
+    private $from_number = '';
+    /**
+     * @return string
+     */
+    private function getFromNumber()
+    {
+        return $this->from_number;
+    }
+    /**
+     * @param string $from_number
+     */
+    public function setFromNumber($from_number)
+    {
+        return $this->from_number = $from_number;
+    }
+
+    /**
+     * @construct
+     */
+    function __construct() {
+
+        $this->dotenv = new \Dotenv\Dotenv(dirname(__DIR__));
+        $this->dotenv->load();
+
+        $this->sid = getenv('TWILIO_SID');
+        $this->token = getenv('TWILIO_TOKEN');
+        $this->from_number = getenv('TWILIO_NUMBER');
+
+        $this->client = new Twilio\Rest\Client($this->getSID(), $this->getToken());
+    }
+    /**
+     * @string $to_number ;
+     * @string $message ;
+     *  $to_number = "218 766 ..."; // Your Auth Token from www.twilio.com/console
+     *  $message = "Hi, this is working!"; // Your Auth Token from www.twilio.com/console
+     */
+    function sendASms($to_number, $message)
+    {
+        $message = $this->client->messages->create(
+            $to_number, // Text this number
+            array(
+                'from' => $this->getFromNumber(), // From a valid Twilio number
+                'body' => $message
+            )
+        );
+        return $message->sid;
+    }
+
+    /**
+     * @string $to_number ;
+     * @string $message ;
+     *  $to_number = "218 766 ..."; // Your Auth Token from www.twilio.com/console
+     *  $message = "Hi, this is working!"; // Your Auth Token from www.twilio.com/console
+     */
+    function sendMultipleSms($to_number, $message)
+    {
+        $message = $this->client->messages->create(
+            $to_number, // Text this number
+            array(
+                'from' => $this->getFromNumber(), // From a valid Twilio number
+                'body' => $message
+            )
+        );
+        return $message->sid;
+    }
+
+    /**
+     * @string $to_number ;
+     *  $to_number = "218 766 ..."; // Your Auth Token from www.twilio.com/console
+     */
+    function call($to_number)
+    {
+        $message = $this->client->calls->create(
+            $to_number, // Call this number
+            $this->getFromNumber(),// From a valid Twilio number
+            array(
+                'url' => 'http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient'
+            )
+        );
+    }
+
+    /**
+     * @var $message
+     * @return $response
+     *  $message = "Hi, this is working!"; // Your Auth Token from www.twilio.com/console
+     */
+    function TwiML($message)
+    {
+        $response = new Twilio\Twiml();
+        $response->say($message);
+        $response->play('https://api.twilio.com/cowbell.mp3', array("loop" => 5));
+        return $response;
+    }
+}
